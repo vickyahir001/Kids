c2c64159151e6fa562037d90d36a4e5a2d2c8ffc
vivek katariya <katariyavivek228@gmail.com>
2024-07-16 12:11:20 +0530

    Initial Commit

---
+++ Kids/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Kids
+//
+//  Created by vivek katariya on 16/07/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
